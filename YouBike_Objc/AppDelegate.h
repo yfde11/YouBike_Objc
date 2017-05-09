6d8f8ed018919aa0d4fbc358873f9ee6a86dd0ac
劉洧熏 <mohocho@liuweixunde-MacBook-Air.local>
2017-05-09 09:41:32 +0800

    Good Start

---
+++ YouBike_Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouBike_Objc
+//
+//  Created by 劉洧熏 on 2017/5/9.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
