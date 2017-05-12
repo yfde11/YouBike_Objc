4986c9be9d792633b4f44b772647433f6a121e31
謝豐任 <volvol@xiefengrende-MacBook-Pro.local>
2017-05-12 17:29:33 +0800

    comment cell

--- YouBike_Objc/AppDelegate.m
+++ YouBike_Objc/AppDelegate.m
@@ -22,6 +22,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [[FBSDKApplicationDelegate sharedInstance] application:application
                              didFinishLaunchingWithOptions:launchOptions];
+    
+    
     return YES;
 }
 
