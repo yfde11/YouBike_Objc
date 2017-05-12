bcb4fcb7e8584292fa71953b2efa9ffc62221a68
劉洧熏 <mohocho@liuweixunde-MacBook-Air.local>
2017-05-12 17:03:37 +0800

    dowload image from oerigin

---
+++ YouBike_Objc/StationsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  StationsTableViewCell.h
+//  YouBike_Objc
+//
+//  Created by 劉洧熏 on 2017/5/12.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StationsTableViewCell : UITableViewCell
+
+@end
---
+++ YouBike_Objc/StationsTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  StationsTableViewCell.m
+//  YouBike_Objc
+//
+//  Created by 劉洧熏 on 2017/5/12.
+//  Copyright © 2017年 劉洧熏. All rights reserved.
+//
+
+#import "StationsTableViewCell.h"
+
+@implementation StationsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
