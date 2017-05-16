1c1a69d9ba68fc53e058411932f025d917394e09
劉洧熏 <mohocho@liuweixunde-MacBook-Air.local>
2017-05-16 15:52:17 +0800

    builded up stations mobel

--- YouBike_Objc/StationInfoController.m
+++ YouBike_Objc/StationInfoController.m
@@ -8,10 +8,11 @@
 
 #import <Foundation/Foundation.h>
 #import "StationsTableViewCell.h"
+#import "StationsCollectionViewCell.h"
 #import "StationInfoController.h"
 #import "Stations.h"
 
-@interface StationInfoController () <UITableViewDelegate, UITableViewDataSource>
+@interface StationInfoController () <UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource>
 
 {
     NSArray<Stations *> *stationsArray;
@@ -21,14 +22,11 @@
 
 @implementation StationInfoController
 
-@synthesize listSwitch;
-@synthesize stationsCollectionView;
-@synthesize stationsTableView;
-
 - (void)viewDidLoad {
 
     [super viewDidLoad];
     [self setUpTableView];
+    [self setUpCollectionView];
     self.stationsCollectionView.hidden = YES;
 
 }
@@ -38,8 +36,26 @@
 
     self.stationsTableView.delegate = self;
     self.stationsTableView.dataSource = self;
-    [stationsTableView registerNib: [UINib nibWithNibName: @"StationsTableViewCell" bundle:nil]
+    [self.stationsTableView registerNib: [UINib nibWithNibName: @"StationsTableViewCell" bundle:nil]
              forCellReuseIdentifier: @"StationsTableViewCell"];
+
+}
+
+-(void)setUpCollectionView
+{
+
+    CGRect screenSize = UIScreen.mainScreen.bounds;
+    self.stationsCollectionView.delegate = self;
+    self.stationsCollectionView.dataSource = self;
+    [self.stationsCollectionView registerNib:[UINib nibWithNibName: @"StationsCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"StationsCollectionViewCell"];
+
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.sectionInset = UIEdgeInsetsMake(14, 14, 14, 14);
+    layout.minimumLineSpacing = 14;
+    layout.minimumInteritemSpacing = 14;
+    layout.itemSize = CGSizeMake((screenSize.size.width - 42) / 2, (screenSize.size.width - 42) / 2);
+
+    self.stationsCollectionView.collectionViewLayout = layout;
 
 }
 
@@ -66,32 +82,42 @@
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+
     StationsTableViewCell *tableViewCell = [tableView dequeueReusableCellWithIdentifier: @"StationsTableViewCell"];
 
-//    if (tableViewCell == nil) {
-//
-//        tableViewCell = [[StationsTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"StationsTableViewCell"];
-//
-//    }
-
     return tableViewCell;
+
 }
 
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+
+    return 20;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    StationsCollectionViewCell *collectionCell = [collectionView dequeueReusableCellWithReuseIdentifier: @"StationsCollectionViewCell" forIndexPath: indexPath];
+
+    return collectionCell;
+
+}
 - (IBAction)switchList:(UISegmentedControl *)sender {
 
-    switch (listSwitch.selectedSegmentIndex) {
+    switch (self.listSwitch.selectedSegmentIndex) {
 
         case 0:
 
-            stationsTableView.hidden = NO;
-            stationsCollectionView.hidden = YES;
+            self.stationsTableView.hidden = NO;
+            self.stationsCollectionView.hidden = YES;
 
             break;
 
         case 1:
 
-            stationsTableView.hidden = YES;
-            stationsCollectionView.hidden = NO;
+            self.stationsTableView.hidden = YES;
+            self.stationsCollectionView.hidden = NO;
 
         default:
 
@@ -99,22 +125,5 @@
 
     }
 }
+
 @end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
